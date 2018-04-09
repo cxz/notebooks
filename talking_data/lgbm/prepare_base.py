c6bf575ada3244b7abda96e1aa6ba69231384682
amaia <alexandre.maia@gmail.com>
2018-04-09 13:15:40 -0300

    cleanup

--- talking_data/lgbm/prepare_base.py
+++ talking_data/lgbm/prepare_base.py
@@ -1,4 +1,4 @@
-""" Convert to Feather
+""" Load base csvs and save as dataframes in feather.
 """
 
 import os
@@ -6,53 +6,55 @@ import gc
 import pickle
 import logging
 import datetime
-from csv import DictReader
-from functools import lru_cache
 from collections import Counter
 from datetime import datetime 
+import logging
 
 import pandas as pd
 import numpy as np
 
 from tqdm import tqdm
-import hashlib
-
-
-TRAIN_ROWS = 184903890
-VALID_ROWS = 53016937 # rows in train.csv with day == 2017-11-09             
-TEST_ROWS_V0 = 57537505
-TEST_ROWS = 18790469
-CACHE = '../cache'
-
-BASE_PATH = '../input'
-TRAIN_CSV = os.path.join(BASE_PATH, 'train.csv')
-TEST_CSV = os.path.join(BASE_PATH, 'test_v0.csv') # v0 with full rows
 
 import feather
 
-TMP = '/kaggle1/td-cache'
+from dataset import *
 
-def convert_to_feather():    
-    """ Load train + val + test df. 
-    """
-    with open(os.path.join(CACHE, 'train_test_base.pkl'), 'rb') as f:
+logger = logging.getLogger("prepare")
 
-        df = pickle.load(f)        
-        df = df.reset_index(drop=True)
-        assert len(df) == TRAIN_ROWS + TEST_ROWS_V0, \
-            "%d %d" %(len(df), TRAIN_ROWS + TEST_ROWS_V0)
+def process(kind):
+    basename = {'train': kind, 'test': 'test_v0'}
+    fname = os.path.join(BASE_PATH, '{}.csv'.format(basename[kind]))
+    logger.info('loading %s' % fname)
+    
+    df = pd.read_csv(fname, dtype=DTYPES, parse_dates=['click_time'])
+    
+    logger.info('hour/day')
+    df['hour'] = df.click_time.dt.hour.astype('uint8')
+    df['day'] = df.click_time.dt.day.astype('uint8')
+    
+    if kind == 'train':
+        df['is_attributed'] = df.is_attributed.astype('uint8')
+    else:
+        df['is_attributed'] = 0
         
-    ips = df[['ip']].values
-    click_times = df[['click_time']].values
-            
-    # lgbm recognizes automatically if the column type is category
-    #categorical = ['app', 'channel', 'device', 'os', 'hour']
-    #for c in categorical:
-    #    df[c] = df[c].astype('category')
+    # recast
+    df.is_attributed = df.is_attributed.fillna(0).astype('uint8')
     
-    train_df = df.iloc[:TRAIN_ROWS]
-    test_df = df.iloc[TRAIN_ROWS:]
-    feather.write_dataframe(train_df, os.path.join(TMP, 'train_base.feather'))
-    feather.write_dataframe(test_df, os.path.join(TMP, 'test_base.feather'))
+    # delete uneeded columns, attributed_time exists only in train
+    # and click_id only in test
+    to_drop = [x for x in ['click_id', 'attributed_time'] if x in df.columns]
+    df.drop(to_drop, axis=1, inplace=True)
     
-convert_to_feather()  
+    df.reset_index(drop=True, inplace=True)
+    
+    logger.info(df.info())
+
+    out_fname = os.path.join(CACHE, '{}_base.feather'.format(kind))
+    logger.info("writing %s" % out_fname)
+    feather.write_dataframe(df, out_fname)
+             
+    logger.info('done.')
+
+
+if __name__ == '__main__':
+    process('train')
