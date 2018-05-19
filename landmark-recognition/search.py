303f0f5b6cbc29e9b4aae36c58c36c6c52a72512
amaia <alexandre.maia@gmail.com>
2018-05-19 14:07:30 -0300

    nearest neighbours using faiss

---
+++ landmark-recognition/search.py
@@ -0,0 +1,68 @@
+""" Build Index with CNN activations
+
+https://github.com/facebookresearch/faiss/wiki/Guidelines-to-choose-an-index
+https://github.com/facebookresearch/faiss/wiki/Indexing-1G-vectors
+https://github.com/facebookresearch/faiss/wiki/Indexing-1M-vectors
+
+nvidia-docker run -ti --name retr-faiss -v /opt/kaggle/landmark-retrieval:/landmark faiss bash
+PYTHONPATH=/opt/faiss python build_index.py
+
+"""
+import time
+import numpy as np
+import faiss
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def htime(c):
+    c = round(c)
+
+    days = c // 86400
+    hours = c // 3600 % 24
+    minutes = c // 60 % 60
+    seconds = c % 60
+
+    if days > 0:
+        return '{}d {}h {}m {}s'.format(days, hours, minutes, seconds)
+    if hours > 0:
+        return '{}h {}m {}s'.format(hours, minutes, seconds)
+    if minutes > 0:
+        return '{}m {}s'.format(minutes, seconds)
+    return '{}s'.format(seconds)
+
+
+def chunks(l, n):
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
+
+
+def search(k_nearest=100):
+    start = time.time()
+
+    index = faiss.read_index('reco.index')
+    print('index loaded')
+
+    x_queries = np.load('reco_qvecs.npy').T
+
+    nearest = np.zeros((x_queries.shape[0], k_nearest), dtype=np.uint32)
+    scores = np.zeros((x_queries.shape[0], k_nearest), dtype=np.uint32)
+
+    chunk_size = 1000
+    for chunk_idx, chunk in enumerate(chunks(range(x_queries.shape[0]), chunk_size)):
+        print(chunk_idx, x_queries.shape[0]//chunk_size)
+        D, I = index.search(x_queries[chunk, :], k_nearest)
+        nearest[chunk, :] = I
+        scores[chunk, :] = D
+
+    np.save("reco_nearest.npy", scores)
+    np.save("reco_nearest_idx.npy", nearest)
+
+    print('done: ', htime(time.time() - start))
+
+
+if __name__ == '__main__':
+    search()
