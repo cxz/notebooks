92a68f24210355e8f26ce4d72ef33a8d0920126d
amaia <alexandre.maia@gmail.com>
2018-05-20 16:46:59 -0300

    search for retr as well

--- landmark-recognition/search.py
+++ landmark-recognition/search.py
@@ -40,12 +40,13 @@ def chunks(l, n):
         yield l[i:i+n]
 
 
-def search(k_nearest=100):
+def search(dataset, k_nearest=100):
     start = time.time()
 
-    index = faiss.read_index('reco.index')
+    index = faiss.read_index('%s.index' % dataset)
     print('index loaded')
 
+    # same queries for both reco & retr datasets
     x_queries = np.load('reco_qvecs.npy').T
 
     nearest = np.zeros((x_queries.shape[0], k_nearest), dtype=np.int64)
@@ -55,14 +56,16 @@ def search(k_nearest=100):
     for chunk_idx, chunk in enumerate(chunks(range(x_queries.shape[0]), chunk_size)):
         print(chunk_idx, x_queries.shape[0]//chunk_size)
         D, I = index.search(x_queries[chunk, :], k_nearest)
+        print(np.min(D), np.max(D), np.min(I), np.max(I))
         nearest[chunk, :] = I
         scores[chunk, :] = D
 
-    np.save("reco_nearest.npy", scores)
-    np.save("reco_nearest_idx.npy", nearest)
+    np.save("%s_nearest.npy" % dataset, scores)
+    np.save("%s_nearest_idx.npy" % dataset, nearest)
 
     print('done: ', htime(time.time() - start))
 
 
 if __name__ == '__main__':
-    search()
+    # search('reco')
+    search('retr')
